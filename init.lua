59573872909a9aff7d97c08f9246aa48648b446e
Tyler Hillery <tyler.p.hillery@outlook.com>
2023-12-29 08:54:49 -0700

    changed theme style

--- init.lua
+++ init.lua
@@ -194,6 +194,9 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
+      require('onedark').setup {
+        style = 'warm'
+      }
       vim.cmd.colorscheme 'onedark'
     end,
   },
